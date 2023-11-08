0f4c3b79942e571f0c2086c1971348c4a52d0441
Tales Mequita <talesmfernandes@gmail.com>
2023-11-08 10:32:34 -0300

    Alteração na Pagina frm_fornecedores.cs
    Deixando os botões do menuStrip funcionais...

--- frm_fornecedor.Designer.cs
+++ frm_fornecedor.Designer.cs
@@ -193,6 +193,7 @@ namespace Projeto_Faculdade
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(99, 20);
             this.toolStripMenuItem1.Text = "Menu Principal";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // cadastroToolStripMenuItem
             // 
@@ -206,14 +207,16 @@ namespace Projeto_Faculdade
             // produtosToolStripMenuItem
             // 
             this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
-            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.produtosToolStripMenuItem.Text = "Produtos";
+            this.produtosToolStripMenuItem.Click += new System.EventHandler(this.produtosToolStripMenuItem_Click);
             // 
             // categoriaToolStripMenuItem
             // 
             this.categoriaToolStripMenuItem.Name = "categoriaToolStripMenuItem";
-            this.categoriaToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.categoriaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.categoriaToolStripMenuItem.Text = "Categoria";
+            this.categoriaToolStripMenuItem.Click += new System.EventHandler(this.categoriaToolStripMenuItem_Click);
             // 
             // relatorioToolStripMenuItem
             // 
@@ -228,20 +231,23 @@ namespace Projeto_Faculdade
             // itemVendas
             // 
             this.itemVendas.Name = "itemVendas";
-            this.itemVendas.Size = new System.Drawing.Size(142, 22);
+            this.itemVendas.Size = new System.Drawing.Size(180, 22);
             this.itemVendas.Text = "Vendas";
+            this.itemVendas.Click += new System.EventHandler(this.itemVendas_Click);
             // 
             // itemFaturamento
             // 
             this.itemFaturamento.Name = "itemFaturamento";
-            this.itemFaturamento.Size = new System.Drawing.Size(142, 22);
+            this.itemFaturamento.Size = new System.Drawing.Size(180, 22);
             this.itemFaturamento.Text = "Faturamento";
+            this.itemFaturamento.Click += new System.EventHandler(this.itemFaturamento_Click);
             // 
             // itemEstoque
             // 
             this.itemEstoque.Name = "itemEstoque";
-            this.itemEstoque.Size = new System.Drawing.Size(142, 22);
+            this.itemEstoque.Size = new System.Drawing.Size(180, 22);
             this.itemEstoque.Text = "Estoque";
+            this.itemEstoque.Click += new System.EventHandler(this.itemEstoque_Click);
             // 
             // button1
             // 
--- frm_fornecedor.cs
+++ frm_fornecedor.cs
@@ -29,5 +29,85 @@ namespace Projeto_Faculdade
             frm.ShowDialog();
             this.Close();
         }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frm_menu frm = new frm_menu();  
+
+            if (this.Visible) 
+            {
+                this.Hide();
+            }
+
+            frm.ShowDialog();
+            this.Close();
+        }
+
+        private void produtosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_produtos frm = new frm_produtos();
+        
+            if (this.Visible) 
+            {
+                this.Hide();
+            }
+
+            frm.ShowDialog();
+            this.Close();
+        }
+
+        private void categoriaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_categorias frm = new frm_categorias();      
+
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+
+            frm.ShowDialog(); 
+            this.Close();
+        }
+
+        private void itemVendas_Click(object sender, EventArgs e)
+        {
+            frm_consulta_pedido frm = new frm_consulta_pedido();
+
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+
+            frm.ShowDialog();
+            this.Close();
+        }
+
+        private void itemFaturamento_Click(object sender, EventArgs e)
+        {
+            frm_faturamento frm = new frm_faturamento();
+
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+
+            frm.ShowDialog();
+
+            this.Close();
+        }
+
+        private void itemEstoque_Click(object sender, EventArgs e)
+        {
+            frm_estoque frm = new frm_estoque();
+
+            if (this.Visible)
+            {
+                this.Hide();
+            }
+
+            frm.ShowDialog();
+
+            this.Close();
+        }
     }
 }
